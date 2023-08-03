871ed53c4fb4d74e57c37b4f355ddd344572814e
X-Tek <support@xtek.repair>
2023-08-03 16:38:24 -0500

    Alphabetized and added descriptions to the functions.

--- xtekpsfunc.ps1
+++ xtekpsfunc.ps1
@@ -1,43 +1,31 @@
 # List of functions1
-function Get-ScriptPath {
-    # If using PowerShell ISE
-    if ($psISE)
-    {
-        $ScriptPath = Split-Path -Parent -Path $psISE.CurrentFile.FullPath
-    }
-    # If using PowerShell 3.0 or greater
-    elseif($PSVersionTable.PSVersion.Major -gt 3)
-    {
-        $ScriptPath = $PSScriptRoot
-    }
-    # If using PowerShell 2.0 or lower
-    else
-    {
-        $ScriptPath = split-path -parent $MyInvocation.MyCommand.Path
-    }
 
-    # If still not found
-    # I found this can happen if running an exe created using PS2EXE module
-    if(-not $ScriptPath) {
-        $ScriptPath = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\')
-    }
+# Converts Powershell style variables to Batch Style Variables
+function ConvertPSVarToBatchVar {
+    param($value)
+    # Find stuff between two () have to escape the () with \
+    $tempvalue = $value | Select-String -Pattern '\(.*\)'
 
-    # Return result
-    return $ScriptPath
+    # Get match value
+    $tempvalue = $tempvalue.Matches.value
+
+    # Save this for later
+    $psvalue = '{0}' -f $tempvalue
+    $psvalue = '$' + $psvalue
+
+    # Get stuff after : before )
+    $tempvalue = $tempvalue.Substring(6,$tempvalue.Length-7)
+
+    # Convert PS Var to Batch Var
+    $batchvalue = '%{0}%' -f $tempvalue
+
+    # Put replace the value with the batch variable
+    $value = $value.replace($psvalue,$batchvalue)
+
+    Return $value
 }
 
-function NetTest {
-    param ($link)
-
-    $X = 0
-        do {
-        Write-Output "Waiting for network"
-        Start-Sleep -s 5
-        $X += 1      
-        } until(($connectresult = Test-Connection $link -Count 3 -Quiet) -eq $True -or $X -eq 3)
-    return $connectresult
-}
-
+# Adds a Debug pause to the script
 function debugpause {
     # If running in the console, wait for input before closing.
     if ($Host.Name -eq "ConsoleHost")
@@ -49,31 +37,7 @@ function debugpause {
 
 }
 
-Function RegChange {
-
-    param ($regkey, $regparam, $regvalue, $regtype)
-    
-    If (-NOT (Test-Path $regkey)) {
-        New-Item -Path $regkey -Force | Out-Null
-    }
-
-    New-ItemProperty -Path $regkey -Name $regparam -Type $regtype -Value $regvalue -Force
-}
-
-function installmsi {
-    param ($filename, $customargs)
-    $scriptdir = Get-ScriptPath
-    $fullfilename = '{0}\{1}' -f $scriptdir,$filename 
-    $logFile = '{0}-{1}.log' -f $filename,$timestamp
-    if ($customargs.count -eq 0) {
-        $MSIArguments = @("/i", ('"{0}"' -f $fullfilename), "/qn", "/norestart", "/L*v", ('"{0}"' -f $logFile))
-    } else {
-        $MSIArguments = @("/i", ('"{0}"' -f $fullfilename), "/qn", "/norestart", "/L*v", ('"{0}"' -f $logFile))
-        $MSIArguments += $customargs
-    }
-    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
-}
-
+# Downloads files with some error checking
 function downloadinstaller {
     param ($downloadfile, $output)
     $outpath = Get-ScriptPath
@@ -102,6 +66,7 @@ function downloadinstaller {
 
 }
 
+# Download latest or pre-release files from a public Git Repository
 function downloadGit {
     param ($repo, $filenamePattern, $preRelease, $output, $getver)
     $outpath = Get-ScriptPath
@@ -150,6 +115,7 @@ function downloadGit {
 
 }
 
+# Download latest file from private repository
 function downloadPrivateGitRepo {
     param(
         $downDir,
@@ -207,3 +173,72 @@ function downloadPrivateGitRepo {
 
     Get-LatestScript
 }
+
+# Returns current scriptpath weather v2.0 or greater powershell
+function Get-ScriptPath {
+    # If using PowerShell ISE
+    if ($psISE)
+    {
+        $ScriptPath = Split-Path -Parent -Path $psISE.CurrentFile.FullPath
+    }
+    # If using PowerShell 3.0 or greater
+    elseif($PSVersionTable.PSVersion.Major -gt 3)
+    {
+        $ScriptPath = $PSScriptRoot
+    }
+    # If using PowerShell 2.0 or lower
+    else
+    {
+        $ScriptPath = split-path -parent $MyInvocation.MyCommand.Path
+    }
+
+    # If still not found
+    # I found this can happen if running an exe created using PS2EXE module
+    if(-not $ScriptPath) {
+        $ScriptPath = [System.AppDomain]::CurrentDomain.BaseDirectory.TrimEnd('\')
+    }
+
+    # Return result
+    return $ScriptPath
+}
+
+# Installs MSI files
+function installmsi {
+    param ($filename, $customargs)
+    $scriptdir = Get-ScriptPath
+    $fullfilename = '{0}\{1}' -f $scriptdir,$filename 
+    $logFile = '{0}-{1}.log' -f $filename,$timestamp
+    if ($customargs.count -eq 0) {
+        $MSIArguments = @("/i", ('"{0}"' -f $fullfilename), "/qn", "/norestart", "/L*v", ('"{0}"' -f $logFile))
+    } else {
+        $MSIArguments = @("/i", ('"{0}"' -f $fullfilename), "/qn", "/norestart", "/L*v", ('"{0}"' -f $logFile))
+        $MSIArguments += $customargs
+    }
+    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
+}
+
+# Completes a simple network test
+function NetTest {
+    param ($link)
+
+    $X = 0
+        do {
+        Write-Output "Waiting for network"
+        Start-Sleep -s 5
+        $X += 1      
+        } until(($connectresult = Test-Connection $link -Count 3 -Quiet) -eq $True -or $X -eq 3)
+    return $connectresult
+}
+
+# Simplifies changing of the registry
+Function RegChange {
+
+    param ($regkey, $regparam, $regvalue, $regtype)
+    
+    If (-NOT (Test-Path $regkey)) {
+        New-Item -Path $regkey -Force | Out-Null
+    }
+
+    New-ItemProperty -Path $regkey -Name $regparam -Type $regtype -Value $regvalue -Force
+}
+
