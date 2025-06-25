f2f4056a9f3eb6a6e152463aaa0a9f3c25041ded
X-Tek <support@xtek.repair>
2025-06-25 16:53:02 -0500

    Updated spelling of chocolatey

--- xtekpsfunc.ps1
+++ xtekpsfunc.ps1
@@ -25,8 +25,8 @@ function ConvertPSVarToBatchVar {
     Return $value
 }
 
-# Install Chocolately
-function installchocolately {
+# Install Chocolatey
+function installchocolatey {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
