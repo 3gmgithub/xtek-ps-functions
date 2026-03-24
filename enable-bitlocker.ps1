4b89acbbea84da718ff17bf45e0ab1f04d85a31a
X-Tek <github@sendmesomething.net>
2026-03-24 16:41:55 -0500

    Added another helper function

---
+++ enable-bitlocker.ps1
@@ -0,0 +1,17 @@
+Function getXfunc {
+    # Download helper functions
+    $xtekpsfuncURL = "https://raw.githubusercontent.com/3gmgithub/xtek-ps-functions/main/xtekpsfunc.ps1"
+
+    If (Test-Path -Path "${scriptdir}\xtekpsfunc.ps1" -PathType Leaf) {
+        Write-Host "${scriptdir}\xtekpsfunc.ps1 already downloaded, skipping..."
+    } else {
+        Write-Host "${scriptdir}\xtekpsfunc.ps1 missing, downloading..."
+        Invoke-WebRequest -Uri $xtekpsfuncURL -OutFile "${scriptdir}\xtekpsfunc.ps1"
+    }
+}
+
+getXfunc
+# Import helper functions
+. ${scriptdir}\xtekpsfunc.ps1
+
+enableBitlocker
